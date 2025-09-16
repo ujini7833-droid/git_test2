661efa98a21e6adad9682a77cf80b4e5dc71108c
Bae, Yujin <ujini7833@gmail.com>
2025-09-16 17:01:54 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,7 +1,18 @@
 package git_test2;
 
 public class Hello {
+	
+	String message;
+	
+	public Hello() {
+		this("안녕하세요");
+	}
+	
+	public Hello(String message) {
+		this.message=message;
+		
+	}
 	public void say() {
-		System.out.println("안녕하세요");	
+		System.out.println(message);
 	}
 }
